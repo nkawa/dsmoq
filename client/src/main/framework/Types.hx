0538458c3dc574ed195abcff7c9561bc7b90e6c7
maeda <maeda@illi-ichi.com>
2014-02-20 16:57:58 +0900

    [Client]PlaceHolderを追加

--- client/src/main/framework/Types.hx
+++ client/src/main/framework/Types.hx
@@ -3,7 +3,7 @@ package framework;
 import promhx.Promise;
 import framework.JQuery;
 
-typedef Html = Dynamic // temporary jQuery object
+typedef Html = Jq
 
 typedef Rendered<State, Out> = {
   html: Html,
@@ -14,6 +14,12 @@ typedef Rendered<State, Out> = {
 typedef Component<In, State, Out> = {
   render: In -> Rendered<State, Out>
 }
+
+enum NextChange<I,O> { Inner(a: I); Outer(b: O); }
+typedef Foldable<In, St, Out> = Component<In, St, NextChange<In, Out>>
+typedef Replacable<In, St, Out> = {> Rendered<St, Out>, put: In -> Void}
+typedef PlaceHolder<In, St, Out> = { render: In -> Replacable<In, St, Out> }
+
 
 typedef Url = String
 
