0679fa55a9e80c346568afb54d47d2a5cbf0637c
maeda <maeda@illi-ichi.com>
2014-02-28 16:36:40 +0900

    [Client]クライアント設定ファイルを追加

---
+++ client/src/main/Settings.hx
@@ -0,0 +1,9 @@
+class Settings{
+    public static var api = new ApiUrl();
+}
+
+private class ApiUrl{
+    public function new(){}
+//    public var login = "/api/login";
+    public var login = "/resources/dummy/api/login.json";
+}
