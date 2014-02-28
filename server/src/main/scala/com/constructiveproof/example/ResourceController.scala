ab993b06a5dfeb2a64cdf3e4cb66805be90dae59
maeda <maeda@illi-ichi.com>
2014-02-28 12:16:18 +0900

    [Server]リソースの種類追加とPOST対応

--- server/src/main/scala/com/constructiveproof/example/ResourceController.scala
+++ server/src/main/scala/com/constructiveproof/example/ResourceController.scala
@@ -5,12 +5,19 @@ import org.scalatra._
 class ResourceController extends ScalatraServlet {
   object Ext{
     val Js = """.*\.js$""".r
+    val Json = """.*\.json$""".r
     val Css = """.*\.css$""".r
     val Html = """.*\.html$""".r
     val Woff = """.*\.woff""".r
     val Ttf =  """.*\.ttf""".r
     val Otf =  """.*\.otf""".r
     val Eot =  """.*\.eot""".r
+    val Jpeg = """.*\.jpe?g$""".r
+    val Png = """.*\.png$""".r
+    val Gif = """.*\.gif$""".r
+    val Zip = """.*\.zip""".r
+    val Txt = """.*\.txt""".r
+    val Csv = """.*\.csv""".r
   }
 
   def resource(filename:String) = new java.io.File(
@@ -24,16 +31,29 @@ class ResourceController extends ScalatraServlet {
   }
 
   get("/resources/(.*)$".r) {
-    val filename = params("captures")
+    returnResource(params("captures"))
+  }
+
+  post("/resources/(.*)$".r) {
+    returnResource(params("captures"))
+  }
+
+  def returnResource(filename: String) = {
     contentType = filename match{
       case Ext.Js() => "application/javascript"
+      case Ext.Json() => "application/json"
       case Ext.Css() => "text/css"
       case Ext.Html() => "text/html"
-      case Ext.Woff() => ""
       case Ext.Ttf() => "application/x-font-ttf"
       case Ext.Otf() => "application/x-font-opentype"
       case Ext.Woff() => "application/font-woff"
       case Ext.Eot() => "application/vnd.ms-fontobject"
+      case Ext.Jpeg() => "image/jpeg"
+      case Ext.Png() => "image/png"
+      case Ext.Gif() => "image/gif"
+      case Ext.Zip() => "application/zip"
+      case Ext.Txt() => "text/plain"
+      case Ext.Csv() => "text/csv"
       case _ => throw new Exception("hoge-")
     }
     resource(filename)
