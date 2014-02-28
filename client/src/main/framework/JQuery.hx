5ee2304eb6021b1a88f8605f1563679d0d778574
maeda <maeda@illi-ichi.com>
2014-02-28 11:18:33 +0900

    [Client]JQueryのメソッド追加

--- client/src/main/framework/JQuery.hx
+++ client/src/main/framework/JQuery.hx
@@ -6,13 +6,18 @@ extern class Jq{
     public function append(jq: Jq):Jq;
     public function add(jq: Jq):Jq;
     public function find(selector: String):Jq;
+    public function filter(selector: String):Jq;
     public function html(text: String):Jq;
+    @:overload(function ():String{})
     public function text(text: String):Jq;
+    @:overload(function (name: String): String{})
     public function attr(name: String, value: String): Jq;
     public function on<A>(event: String, f: A->Void): Jq;
     public function empty(): Jq;
     public function addClass(name: String): Jq;
-    public function val(): String;
+    @:overload(function ():String{})
+    public function val(value: String): Jq;
+    public var length: Int;
 }
 
 class JQuery{
@@ -22,6 +27,15 @@ class JQuery{
 
     public static function div(){ return j('<div></div>'); }
     public static function divNamed(name){ return j('<div></div>').attr("name", name); }
+
+    public static function findAll(jq:Html, selector){
+        var ret = jq.filter(selector);
+        return if(ret.length == 0){
+            jq.find(selector);
+        }else{
+            ret;
+        }
+    }
 
     public static function join(tag: String): Array<Jq> -> Jq{
         return function(htmls:Array<Jq>){
