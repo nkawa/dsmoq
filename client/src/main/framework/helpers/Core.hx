71bdc9b690421b4ad6bfef218f92603c334a94d7
maeda <maeda@illi-ichi.com>
2014-02-28 11:19:14 +0900

    [Client]オブジェクトをマージする関数作成

--- client/src/main/framework/helpers/Core.hx
+++ client/src/main/framework/helpers/Core.hx
@@ -4,4 +4,17 @@ class Core{
     public static function identity(x){return x;}
     public static function effect(f){return function(x){f(x); return x;};}
     public static function nop<A>():A{ throw "nop called"; return null;}
+
+    public static function merge<A>(a: Dynamic, b: Dynamic):A{
+        function copy(org: Dynamic, o: Dynamic){
+            Lambda.iter(Reflect.fields(o), function(fieldName){
+                var value = Reflect.field(o, fieldName);
+                Reflect.setField(org, fieldName, value);
+            });
+        }
+        var ret = untyped {};
+        copy(ret, a);
+        copy(ret, b);
+        return ret;
+    }
 }
