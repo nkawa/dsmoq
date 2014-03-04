712408af19212fd4c142645c3c5f178144f820e4
Takuya OKADA <t_okada@denkiyagi.jp>
2014-03-04 16:36:16 +0900

    [Server]ScalikeJDBC、JSON4Sライブラリを追加

--- server/project/build.scala
+++ server/project/build.scala
@@ -25,7 +25,14 @@ object ScentryauthdemoBuild extends Build {
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
         "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
         "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
-        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
+        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar")),
+        "org.scalatest" % "scalatest_2.10" % "2.1.0" % "test",
+        "org.postgresql" % "postgresql" % "9.3-1101-jdbc41",
+        "org.scalikejdbc" %% "scalikejdbc" % "[1.7,)",
+        "org.scalikejdbc" %% "scalikejdbc-interpolation" % "[1.7,)",
+        "org.scalikejdbc" %% "scalikejdbc-test" % "[1.7,)" % "test",
+        "org.scalatra" %% "scalatra-json" % "2.2.2",
+        "org.json4s" %% "json4s-jackson" % "3.2.6"
       )
     )
   )
