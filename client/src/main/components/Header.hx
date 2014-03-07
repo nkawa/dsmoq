ba5c02166267a485ae12964a61436898cc3b4f12
maeda <maeda@illi-ichi.com>
2014-03-07 17:43:41 +0900

    [Client]ログイン/ログアウト後にリフレッシュするように

--- client/src/main/components/Header.hx
+++ client/src/main/components/Header.hx
@@ -4,7 +4,9 @@ import pages.Auth;
 import framework.helpers.*;
 using framework.helpers.Components;
 import framework.Types;
+import framework.JQuery;
 import pushstate.PushState;
+import js.Browser.document in doc;
 
 private typedef HeaderModel = {
     login: Bool,
@@ -34,9 +36,23 @@ class Header{
             Lambda.iter(links, function(t){
                 html.find(t.selector).on("click", function(_){ PushState.push(t.url);});
             });
+
+            clickToSubmit(html, "[data-submit-login]",  "[data-link-login]",  Settings.api.login);
+            clickToSubmit(html, "[data-submit-logout]", "[data-link-logout]", Settings.api.logout);
+
             return html;
         }
 
         return Templates.create("header").inMap(toModel).stateMap(Core.ignore).decorate(after);
+    }
+
+    private static function clickToSubmit(html: Html, formSelector, clickSelector, url){
+        JQuery.findAll(html, clickSelector).on("click", function(_){
+            var jqXHR = (untyped JQuery.findAll(html, formSelector)).ajaxSubmit({url: url, dataType:"JSON"}).data('jqxhr');
+            // TODO: Error handling
+            jqXHR.done(function(_){
+                doc.location.reload();
+            });
+        });
     }
 }
