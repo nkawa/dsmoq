3454dc811b98f8fe54b078cfff10b0153fa539e5
maeda <maeda@illi-ichi.com>
2014-03-07 11:27:24 +0900

    [Client]共通の通信コンポーネントを追加

--- client/src/main/pages/Common.hx
+++ client/src/main/pages/Common.hx
@@ -1,7 +1,16 @@
 package pages;
+import framework.Types;
+import components.ConnectionPanel;
 
 class Common{
     public static function waiting(html){
         html.text("waiting...");
+    }
+
+    public static function connectionPanel<Output>(
+        name,
+        component: Component<Json, Void, Output>
+    ){
+        return ConnectionPanel.create.bind(waiting);
     }
 }
