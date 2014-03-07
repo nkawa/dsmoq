b8a75a89104603bc78d614705bef15ee1cd68e11
maeda <maeda@illi-ichi.com>
2014-03-07 17:52:56 +0900

    [Client]Settingにlogout用のURLを追加

--- client/src/main/Settings.hx
+++ client/src/main/Settings.hx
@@ -4,7 +4,9 @@ class Settings{
 
 private class ApiUrl{
     public function new(){}
-//    public var login = "/api/login";
+//    public var login = "/api/signin";
     public var login = "/resources/dummy/api/login.json";
 //    public var login = "/resources/dummy/api/not_login.json";
+//    public var logout = "/api/signout";
+    public var logout = "/resources/dummy/api/login.json";
 }
