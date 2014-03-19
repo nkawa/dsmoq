7a8aaff3085e0603cd778a3c022dbbc2d82705f4
terurou <terurou@denkiyagi.jp>
2014-03-19 18:59:49 +0900

    [Server]ScalikeJDBC/QueryDSLのPostgreSQL UUID用のヘルパーを追加

---
+++ server/src/main/scala/dsmoq/models/PostgresqlHelper.scala
@@ -0,0 +1,35 @@
+package dsmoq.models
+
+import scalikejdbc.SQLInterpolation._
+
+/**
+ * Created by terurou on 14/03/19.
+ */
+object PostgresqlHelper {
+
+  implicit class UuidConditionSQLBuilder[A](val self: ConditionSQLBuilder[A]) extends AnyVal {
+    def inByUuid(column: SQLSyntax, values: Seq[String]): ConditionSQLBuilder[A] = self.append(sqls.inByUuid(column, values))
+    def notInByUuid(column: SQLSyntax, values: Seq[String]): ConditionSQLBuilder[A] = self.append(sqls.notInByUuid(column, values))
+  }
+
+  implicit class UuidSQLSyntax(val self: sqls.type) extends AnyVal {
+    def uuid(value: String): SQLSyntax = sqls"UUID(${value})"
+
+    def inByUuid(column: SQLSyntax, values: Seq[String]) = {
+      if (values.nonEmpty) {
+        sqls"${column} in ( ${sqls.join(values.map(x => sqls.uuid(x)), sqls",")} )"
+      } else {
+        sqls""
+      }
+    }
+
+    def notInByUuid(column: SQLSyntax, values: Seq[String]) = {
+      if (values.nonEmpty) {
+        sqls"${column} not in ( ${sqls.join(values.map(x => sqls.uuid(x)), sqls",")} )"
+      } else {
+        sqls""
+      }
+    }
+  }
+
+}
