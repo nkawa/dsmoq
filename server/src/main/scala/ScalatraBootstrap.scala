51a29ebdd3e538531ea0c182426ce0124125e302
terurou <terurou@denkiyagi.jp>
2014-03-19 18:58:27 +0900

    [Server]ScalikeJDBC 実行SQLをトレースできるようにした

--- server/src/main/scala/ScalatraBootstrap.scala
+++ server/src/main/scala/ScalatraBootstrap.scala
@@ -2,10 +2,17 @@ import dsmoq._
 import org.scalatra._
 import javax.servlet.ServletContext
 import scalikejdbc.config.DBs
+import scalikejdbc._
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
+    GlobalSettings.loggingSQLAndTime = LoggingSQLAndTimeSettings(
+      enabled = true,
+      singleLineMode = true,
+      logLevel = 'DEBUG
+    )
     DBs.setup()
+
     context.mount(new ResourceController, "/*")
     context.mount(new SessionsController, "/sessions/*")
     context.mount(new ServerApiController, "/api/*")
