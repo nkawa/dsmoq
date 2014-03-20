063fb0fb95a6e8920cc9760715165d7aae1abb95
maeda <maeda@illi-ichi.com>
2014-03-20 15:23:51 +0900

    [Client]ACL変更用のURL設定を追加

--- client/src/main/Settings.hx
+++ client/src/main/Settings.hx
@@ -19,4 +19,25 @@ private class ApiUrl{
 //    public var login = "/resources/dummy/api/not_login.json";
     public var logout = "/api/signout";
 //    public var logout = "/resources/dummy/api/login.json";
+
+    /*
+    public function datasetAddAcl(datasetId: String){
+        return '/api/datasets/$datasetId/acl';
+    }
+    public function datasetChangeAcl(datasetId: String, itemId: String){
+        return '/api/datasets/$datasetId/acl/$itemId';
+    }
+    public function datasetRemoveAcl(datasetId: String, itemId: String){
+        return '/api/datasets/$datasetId/acl/$itemId';
+    }
+    */
+    public function datasetAddAcl(datasetId: String){
+        return "/resources/dummy/api/acl_ok.json";
+    }
+    public function datasetChangeAcl(datasetId: String, itemId: String){
+        return "/resources/dummy/api/ok.json";
+    }
+    public function datasetRemoveAcl(datasetId: String, itemId: String){
+        return "/resources/dummy/api/ok.json";
+    }
 }
