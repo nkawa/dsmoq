ba6221b8ce8c4f400eda10cfff03f41680e186b0
maeda <maeda@illi-ichi.com>
2014-03-21 15:15:45 +0900

    [Client]スタブのページを配置

--- client/src/main/pages/Definitions.hx
+++ client/src/main/pages/Definitions.hx
@@ -4,15 +4,15 @@ import framework.Types;
 import promhx.Promise;
 import framework.JQuery.*;
 import components.Clickable;
-import pages.DashBoardView;
 
 import framework.helpers.*;
 import framework.Effect;
 import components.Pagination;
 
-enum Page { DashBoard; 
-    DatasetList(paging: Option<PagingRequest>); DatasetNew; DatasetRead(id: String); DatasetEdit(id: String); 
-    GroupList; Profile; }
+enum Page { DashBoard;
+    DatasetList(paging: Option<PagingRequest>); DatasetNew; DatasetRead(id: String); DatasetEdit(id: String);
+    GroupList; GroupRead; GroupEdit;
+    Profile; }
 
 class Definitions{
     public static function application(){
@@ -23,8 +23,10 @@ class Definitions{
                     case DatasetRead(id):       Address.url('/datasets/read/$id');
                     case DatasetEdit(id):       Address.url('/datasets/edit/$id');
                     case DatasetList(paging):   Address.url('/datasets/list/', paging);
-                    case GroupList:             Address.url('/groups/list/');
                     case DatasetNew:            Address.url('/datasets/new/');
+                    case GroupList:             Address.url('/groups/list/');
+                    case GroupRead:             Address.url('/groups/read/');
+                    case GroupEdit:             Address.url('/groups/edit/');
                     case Profile:               Address.url('/profile/');
                 }
             },
@@ -33,21 +35,25 @@ class Definitions{
                     case ["datasets", "read", id]:    DatasetRead(id);
                     case ["datasets", "edit", id]:    DatasetEdit(id);
                     case ["datasets", "list"]:      DatasetList(untyped Address.hash(url));  // TODO: how type it?
-                    case ["groups", "list"]:        GroupList;
                     case ["datasets", "new"]:       DatasetNew;
+                    case ["groups", "list"]:        GroupList;
+                    case ["groups", "read"]:        GroupRead;
+                    case ["groups", "edit"]:        GroupEdit;
                     case ["profile"]:               Profile;
                     case _:                         DashBoard;
                 }
             },
             draw: function(page: Page){
                 var body = switch(page){
-                    case DashBoard:                 DashBoardView.render();
+                    case DashBoard:                 Stub.render("DashBoard");
                     case DatasetRead(id):           DatasetReadView.render(id);
                     case DatasetEdit(id):           DatasetEditView.render(Some(id));
                     case DatasetNew:                DatasetEditView.render(None);
                     case DatasetList(paging):       DatasetListView.render(Core.getOrElse(paging, Pagination.top));
-                    case GroupList:                 DashBoardView.render();
-                    case Profile:                   DashBoardView.render();
+                    case GroupList:                 Stub.render("GroupList");
+                    case GroupRead:                 Stub.render("GroupRead");
+                    case GroupEdit:                 Stub.render("GroupEdit");
+                    case Profile:                   Stub.render("Profile");
                 }
                 return { html: body.html, state: Core.nop, event: body.event };
             }
