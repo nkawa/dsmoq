c98296d0c7f2562f2a394358e825bc59ade4d2a1
Takuya OKADA <t_okada@denkiyagi.jp>
2014-04-04 16:33:13 +0900

    [Server]Googleアカウントでログイン時にユーザーが存在しなければ新しく作成するようにした

--- server/src/main/scala/dsmoq/controllers/MockController.scala
+++ server/src/main/scala/dsmoq/controllers/MockController.scala
@@ -6,9 +6,14 @@ import com.google.api.client.googleapis.auth.oauth2.{GoogleAuthorizationCodeRequ
 import com.google.api.client.json.jackson.JacksonFactory
 import java.util
 import com.google.api.services.oauth2.Oauth2
+import java.util.UUID
+import org.joda.time.DateTime
 
+// あとで消す
+import scalikejdbc._, SQLInterpolation._
+import dsmoq.{AppConf, persistence}
 
-class MockController extends ScalatraServlet {
+class MockController extends ScalatraServlet with SessionTrait {
   get("/") {
     <html>
       <body>
@@ -27,6 +32,7 @@ class MockController extends ScalatraServlet {
           <p><input type="submit" value="Upload" /></p>
         </form>
 
+        <h2>Google Account OAuth 2.0</h2>
         <form action="/mock/login_google" method="get">
           <input type="hidden" name="path" value="/mock" />
           <p><input type="submit" value="Login" /></p>
@@ -39,8 +45,8 @@ class MockController extends ScalatraServlet {
     // TODO パラメーターチェック(必要なら)、エラー処理
     println(params("path"))
 
-    val clientId = "770034855439.apps.googleusercontent.com";
-    val callbackUrl = "http://localhost:8080/mock/callback";
+    val clientId = "770034855439.apps.googleusercontent.com"
+    val callbackUrl = "http://localhost:8080/mock/callback"
     val scopes = util.Arrays.asList("https://www.googleapis.com/auth/plus.me", "profile", "email")
     val userBackUri = params("path")
 
@@ -50,6 +56,7 @@ class MockController extends ScalatraServlet {
 
   get("/callback") {
     // TODO パラメーターチェック(必要ならCSRFチェック)、エラー処理
+    // 認証拒否された時、DB接続エラー時、APIコールでエラー時etc
     println(params)
 
      // 固有設定系はファイルに避ける予定
@@ -57,7 +64,7 @@ class MockController extends ScalatraServlet {
     val clientSecret = "stTAYEg6CVW6pj7Mab3SgoGm";
     val callbackUrl = "http://localhost:8080/mock/callback";
     val scopes = util.Arrays.asList("https://www.googleapis.com/auth/plus.me", "profile", "email")
-    val applicationName = "testproj"
+    val applicationName = "COI Data Store"
 
     val userRedirectUri = params("state")
     val authenticationCode = params("code")
@@ -70,14 +77,115 @@ class MockController extends ScalatraServlet {
     // call google api : get user information (name & e-mail)
     val oauth2 = new Oauth2.Builder(credential.getTransport, credential.getJsonFactory, credential).setApplicationName(applicationName).build()
     val user = oauth2.userinfo().get().execute()
+    println("debug:" + user)
     println("name:" + user.getName)
     println("email:" + user.getEmail)
     println("id:" + user.getId)
 
     // ユーザーがなければユーザー作成、ユーザー情報を引きセッション作成etc
+    // やっつけ mail_addressesとはjoinしていない また、tryでくくる必要あり
+    val result = DB readOnly { implicit session =>
+      sql"""
+        SELECT
+          users.*
+        FROM
+          users
+        INNER JOIN
+          google_users
+        ON
+          users.id = google_users.user_id
+        WHERE
+          google_users.google_user_id = ${user.getId}
+      """.map(_.toMap()).single().apply()
+    }
 
-//    redirect(userRedirectUri)
-    redirect("/mock/finish")
+    val bbb = result match {
+      case Some(x) =>
+        // FIXME 暫定 あとで↑のSQLとともに直す
+        dsmoq.services.data.User(
+          id = result.get("id").toString,
+          name = result.get("name").toString,
+          fullname = result.get("fullname").toString,
+          organization = result.get("organization").toString,
+          title = result.get("title").toString,
+          image = "",
+          isGuest = false,
+          isDeleted = false
+        )
+      case None =>
+        // ユーザー作成
+        val timestamp = DateTime.now()
+        val aaa = DB localTx {
+          implicit s =>
+            // uuid case helperがなかったので暫定的にModel修正(自動生成時に何とかなるのか？)
+            val username = user.getEmail split '@'
+            val u = persistence.User.create(
+              id = UUID.randomUUID.toString,
+              name = username(0),
+              fullname = user.getName,
+              organization = "",
+              title = "",
+              description = "Google Apps User",
+              imageId = AppConf.defaultDatasetImageId,
+              createdBy = AppConf.systemUserId,
+              createdAt = timestamp,
+              updatedBy = AppConf.systemUserId,
+              updatedAt = timestamp
+            )
+            val mail = persistence.MailAddress.create(
+              id = UUID.randomUUID.toString,
+              userId = u.id,
+              address = user.getEmail,
+              status = 1,
+              createdBy = AppConf.systemUserId,
+              createdAt = timestamp,
+              updatedBy = AppConf.systemUserId,
+              updatedAt = timestamp
+            )
+            // やっつけ google_usersに保存
+            sql"""
+            INSERT INTO
+              google_users(id, user_id, google_user_id, created_by, created_at, updated_by, updated_at)
+            VALUES
+              (UUID(${UUID.randomUUID.toString}), UUID(${u.id}), ${user.getId}, UUID(${AppConf.systemUserId}), ${timestamp}, UUID(${AppConf.systemUserId}), ${timestamp})
+            """.update().apply()
+            // groupの作成
+            val g = persistence.Group.create(
+              id = UUID.randomUUID.toString,
+              name = username(0),
+              description = "",
+              groupType = 1,
+              createdBy = AppConf.systemUserId,
+              createdAt = timestamp,
+              updatedBy = AppConf.systemUserId,
+              updatedAt = timestamp
+            )
+            // memberの作成
+            val m = persistence.Member.create(
+              id = UUID.randomUUID.toString,
+              groupId = g.id,
+              userId = u.id,
+              role = 1,
+              status = 1,
+              createdBy = AppConf.systemUserId,
+              createdAt = timestamp,
+              updatedBy = AppConf.systemUserId,
+              updatedAt = timestamp
+            )
+            u
+        }
+        dsmoq.services.data.User(aaa)
+    }
+
+    // セッション作成
+    clearSession()
+    setUserInfoToSession(bbb)
+
+    // 元いたページに戻す
+    redirect(userRedirectUri)
+//    redirect("/mock/finish")
+//    redirect("/mock")
+//    redirect("/datasets/list")
   }
 
   get("/finish") {
