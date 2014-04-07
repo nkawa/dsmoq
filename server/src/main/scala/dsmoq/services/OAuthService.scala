5cf2660626be7e12518ebc377bf0041859ea397c
Takuya OKADA <t_okada@denkiyagi.jp>
2014-04-07 12:01:28 +0900

    [Server]ユーザー作成処理を別メソッドに分けた

--- server/src/main/scala/dsmoq/services/OAuthService.scala
+++ server/src/main/scala/dsmoq/services/OAuthService.scala
@@ -8,6 +8,7 @@ import com.google.api.services.oauth2.Oauth2
 import org.joda.time.DateTime
 import java.util.UUID
 import scalikejdbc._, SQLInterpolation._
+import com.google.api.services.oauth2.model.Userinfoplus
 
 object OAuthService {
   def getAuthenticationUrl(location: String) = {
@@ -20,18 +21,29 @@ object OAuthService {
   }
   
   def loginWithGoogle(authenticationCode: String) = {
-    // get access token
-    val flow = new GoogleAuthorizationCodeFlow(new NetHttpTransport(), new JacksonFactory(), OAuthConf.clientId, OAuthConf.clientSecret, OAuthConf.scopes);
-    val tokenResponse = flow.newTokenRequest(authenticationCode).setRedirectUri(OAuthConf.callbackUrl).execute();
+    // 認証トークンからアクセストークン取得
+    val flow = new GoogleAuthorizationCodeFlow(
+      new NetHttpTransport(),
+      new JacksonFactory(),
+      OAuthConf.clientId,
+      OAuthConf.clientSecret,
+      OAuthConf.scopes)
+    val tokenResponse = flow.newTokenRequest(authenticationCode)
+      .setRedirectUri(OAuthConf.callbackUrl).execute();
     val credential = flow.createAndStoreCredential(tokenResponse, null);
 
-    // call google api : get user information (name & e-mail)
-    val oauth2 = new Oauth2.Builder(credential.getTransport, credential.getJsonFactory, credential).setApplicationName(OAuthConf.applicationName).build()
-    val user = oauth2.userinfo().get().execute()
-    println("debug:" + user)
-    println("name:" + user.getName)
-    println("email:" + user.getEmail)
-    println("id:" + user.getId)
+    val oauth2 = new Oauth2.Builder(
+      credential.getTransport,
+      credential.getJsonFactory,
+      credential)
+      .setApplicationName(OAuthConf.applicationName).build()
+    val googleUser = oauth2.userinfo().get().execute()
+
+    // デバッグ用
+    println("debug:" + googleUser)
+    println("name:" + googleUser.getName)
+    println("email:" + googleUser.getEmail)
+    println("id:" + googleUser.getId)
 
     // ユーザーがなければユーザー作成、ユーザー情報を引きセッション作成etc
     // やっつけ mail_addressesとはjoinしていない また、tryでくくる必要あり
@@ -46,11 +58,11 @@ object OAuthService {
         ON
           users.id = google_users.user_id
         WHERE
-          google_users.google_user_id = ${user.getId}
+          google_users.google_id = ${googleUser.getId}
       """.map(_.toMap()).single().apply()
     }
 
-    val bbb = result match {
+    val user = result match {
       case Some(x) =>
         // FIXME 暫定 あとで↑のSQLとともに直す
         dsmoq.services.data.User(
@@ -63,69 +75,72 @@ object OAuthService {
           isGuest = false,
           isDeleted = false
         )
-      case None =>
-        // ユーザー作成
-        val timestamp = DateTime.now()
-        val aaa = DB localTx {
-          implicit s =>
-            val username = user.getEmail split '@'
-            val u = persistence.User.create(
-              id = UUID.randomUUID.toString,
-              name = username(0),
-              fullname = user.getName,
-              organization = "",
-              title = "",
-              description = "Google Apps User",
-              imageId = AppConf.defaultDatasetImageId,
-              createdBy = AppConf.systemUserId,
-              createdAt = timestamp,
-              updatedBy = AppConf.systemUserId,
-              updatedAt = timestamp
-            )
-            val mail = persistence.MailAddress.create(
-              id = UUID.randomUUID.toString,
-              userId = u.id,
-              address = user.getEmail,
-              status = 1,
-              createdBy = AppConf.systemUserId,
-              createdAt = timestamp,
-              updatedBy = AppConf.systemUserId,
-              updatedAt = timestamp
-            )
-            // やっつけ google_usersに保存
-            sql"""
-            INSERT INTO
-              google_users(id, user_id, google_user_id, created_by, created_at, updated_by, updated_at)
-            VALUES
-              (UUID(${UUID.randomUUID.toString}), UUID(${u.id}), ${user.getId}, UUID(${AppConf.systemUserId}), ${timestamp}, UUID(${AppConf.systemUserId}), ${timestamp})
-            """.update().apply()
-            // groupの作成
-            val g = persistence.Group.create(
-              id = UUID.randomUUID.toString,
-              name = username(0),
-              description = "",
-              groupType = 1,
-              createdBy = AppConf.systemUserId,
-              createdAt = timestamp,
-              updatedBy = AppConf.systemUserId,
-              updatedAt = timestamp
-            )
-            // memberの作成
-            val m = persistence.Member.create(
-              id = UUID.randomUUID.toString,
-              groupId = g.id,
-              userId = u.id,
-              role = 1,
-              status = 1,
-              createdBy = AppConf.systemUserId,
-              createdAt = timestamp,
-              updatedBy = AppConf.systemUserId,
-              updatedAt = timestamp
-            )
-            u
-        }
-        dsmoq.services.data.User(aaa)
+      case None => createUser(googleUser)
     }
-    bbb
+    user
+  }
+
+  private def createUser(googleUser: Userinfoplus) = {
+    val username = googleUser.getEmail.split('@')(0)
+    val timestamp = DateTime.now()
+    val dbUser = DB localTx { implicit s =>
+      // insert users
+      val u = persistence.User.create(
+        id = UUID.randomUUID.toString,
+        name = username,
+        fullname = googleUser.getName,
+        organization = "",
+        title = "",
+        description = "Google Apps User",
+        imageId = AppConf.defaultDatasetImageId,
+        createdBy = AppConf.systemUserId,
+        createdAt = timestamp,
+        updatedBy = AppConf.systemUserId,
+        updatedAt = timestamp
+      )
+      // insert mail_addresses
+      persistence.MailAddress.create(
+        id = UUID.randomUUID.toString,
+        userId = u.id,
+        address = googleUser.getEmail,
+        status = 1,
+        createdBy = AppConf.systemUserId,
+        createdAt = timestamp,
+        updatedBy = AppConf.systemUserId,
+        updatedAt = timestamp
+      )
+      // FIXME insert google_users
+      sql"""
+          INSERT INTO
+            google_users(id, user_id, google_id, created_by, created_at, updated_by, updated_at)
+          VALUES
+            (UUID(${UUID.randomUUID.toString}), UUID(${u.id}), ${googleUser.getId}, UUID(${AppConf.systemUserId}), ${timestamp}, UUID(${AppConf.systemUserId}), ${timestamp})
+          """.update().apply()
+      // insert groups
+      val g = persistence.Group.create(
+        id = UUID.randomUUID.toString,
+        name = username,
+        description = "",
+        groupType = 1,
+        createdBy = AppConf.systemUserId,
+        createdAt = timestamp,
+        updatedBy = AppConf.systemUserId,
+        updatedAt = timestamp
+      )
+      // insert members
+      persistence.Member.create(
+        id = UUID.randomUUID.toString,
+        groupId = g.id,
+        userId = u.id,
+        role = 1,
+        status = 1,
+        createdBy = AppConf.systemUserId,
+        createdAt = timestamp,
+        updatedBy = AppConf.systemUserId,
+        updatedAt = timestamp
+      )
+      u
+    }
+    dsmoq.services.data.User(dbUser)
   }
 }
