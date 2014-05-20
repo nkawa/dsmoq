a85975117fc41f966f99fd4a865d31d844b0440c
Takuya OKADA <t_okada@denkiyagi.jp>
2014-05-20 17:46:43 +0900

    [Server]ファイルダウンロードAPIの実装
    ダウンロードURLは、 /images/:datasetId/:imageId

---
+++ server/src/main/scala/dsmoq/controllers/FileController.scala
@@ -0,0 +1,27 @@
+package dsmoq.controllers
+
+import dsmoq.services.FileService
+import scala.util.{Failure, Success}
+import org.scalatra.{ScalatraServlet, NotFound}
+import org.scalatra.servlet.FileUploadSupport
+
+class FileController extends ScalatraServlet with SessionTrait with FileUploadSupport {
+
+  get("/:datasetId/:id") {
+    val datasetId = params("datasetId")
+    val id = params("id")
+
+    val result = for {
+      user <- getUserInfoFromSession()
+      fileInfo <- FileService.getFile(datasetId, id, user)
+    } yield {
+      fileInfo
+    }
+    result match {
+      case Success(x) =>
+        response.setHeader("Content-Disposition", "attachment; filename=" + x._2)
+        x._1
+      case Failure(e) => halt(status = 403, reason = "Forbidden", body="Forbidden")
+    }
+  }
+}
---
+++ server/src/main/scala/dsmoq/services/FileService.scala
@@ -0,0 +1,81 @@
+package dsmoq.services
+
+import scalikejdbc._, SQLInterpolation._
+import dsmoq.{AppConf, persistence}
+import dsmoq.persistence.PostgresqlHelper._
+import scala.util.{Failure, Success}
+import java.nio.file.Paths
+import dsmoq.services.data.User
+
+object FileService {
+  def getFile(datasetId: String, fileId: String, user: User) = {
+    try {
+      val fileInfo = DB readOnly { implicit s =>
+        // 権限によりダウンロード可否の決定
+        val permission = if (user.isGuest) {
+          getGuestPermission(datasetId)
+        } else {
+          getUserPermission(datasetId, user.id)
+        }
+        if (permission < 2) {
+          throw new RuntimeException("access denied")
+        }
+
+        val file = persistence.File.find(fileId)
+        val fh = persistence.FileHistory.syntax("fi")
+        val fileHistory = withSQL {
+          select(fh.result.id)
+            .from(persistence.FileHistory as fh)
+            .where
+            .eq(fh.fileId, sqls.uuid(fileId))
+            .and
+            .isNull(fh.deletedAt)
+        }.map(rs => rs.string(fh.resultName.id)).single().apply
+        file match {
+          case Some(f) => (f, fileHistory.get)
+          case None => throw new RuntimeException("data not found.")
+        }
+      }
+
+      val filePath = Paths.get(AppConf.fileDir, datasetId, fileId, fileInfo._2).toFile
+      if (!filePath.exists()) throw new RuntimeException("file not found")
+
+      val file = new java.io.File(filePath.toString)
+      Success((file, fileInfo._1.name))
+    } catch {
+      case e: Exception => Failure(e)
+    }
+  }
+
+  private def getGuestPermission(datasetId: String)(implicit s: DBSession) = {
+    val o = persistence.Ownership.syntax("o")
+    withSQL {
+      select(o.result.accessLevel)
+        .from(persistence.Ownership as o)
+        .where
+        .eq(o.datasetId, sqls.uuid(datasetId))
+        .and
+        .eq(o.groupId, sqls.uuid(AppConf.guestGroupId))
+        .and
+        .isNull(o.deletedAt)
+    }.map(_.int(o.resultName.accessLevel)).single().apply().getOrElse(0)
+  }
+
+  private def getUserPermission(datasetId: String, userId: String)(implicit s: DBSession) = {
+    val o = persistence.Ownership.syntax("o")
+    val g = persistence.Group.syntax("g")
+    val m = persistence.Member.syntax("m")
+    withSQL {
+      select(o.result.accessLevel)
+        .from(persistence.Ownership as o)
+        .innerJoin(persistence.Group as g).on(o.groupId, g.id)
+        .innerJoin(persistence.Member as m).on(g.id, m.groupId)
+        .where
+        .eq(o.datasetId, sqls.uuid(datasetId))
+        .and
+        .eq(m.userId, sqls.uuid(userId))
+        .and
+        .isNull(o.deletedAt)
+    }.map(_.int(o.resultName.accessLevel)).single().apply().getOrElse(0)
+  }
+}
