72cac54bd268add27452c5b4e4283b062026af09
Takuya OKADA <t_okada@denkiyagi.jp>
2014-05-29 12:22:06 +0900

    [Server]データセットAPIのテストを追加
    一部テストは未実装

---
+++ server/src/test/scala/api/DatasetApiSpec.scala
@@ -0,0 +1,344 @@
+package api
+
+import org.scalatest.{BeforeAndAfter, FreeSpec}
+import org.scalatra.test.scalatest.ScalatraSuite
+import org.json4s.{DefaultFormats, Formats}
+import dsmoq.controllers.{ImageController, FileController, ApiController}
+import scalikejdbc.config.DBs
+import org.json4s.jackson.JsonMethods._
+import java.io.File
+import dsmoq.services.data.DatasetData._
+import dsmoq.AppConf
+import org.scalatra.servlet.MultipartConfig
+import dsmoq.services.data.DatasetData.DatasetDeleteImage
+import dsmoq.services.data.DatasetData.DatasetAddFiles
+import dsmoq.services.data.DatasetData.Dataset
+import scala.Some
+import dsmoq.controllers.AjaxResponse
+import dsmoq.services.data.DatasetData.DatasetAddImages
+import dsmoq.services.data.RangeSlice
+
+class DatasetApiSpec extends FreeSpec with ScalatraSuite with BeforeAndAfter {
+  protected implicit val jsonFormats: Formats = DefaultFormats
+
+  private val dummyFile = new File("README.md")
+  private val dummyImage = new File("../client/www/dummy/images/nagoya.jpg")
+
+  // multi-part file upload config
+  val holder = addServlet(classOf[ApiController], "/api/*")
+  holder.getRegistration.setMultipartConfig(
+    MultipartConfig(
+      maxFileSize = Some(3 * 1024 * 1024),
+      fileSizeThreshold = Some(1 * 1024 * 1024)
+    ).toMultipartConfigElement
+  )
+  addServlet(classOf[FileController], "/files/*")
+  addServlet(classOf[ImageController], "/images/*")
+
+  before {
+    DBs.setup()
+  }
+  after {
+    DBs.close()
+  }
+
+  "API test" - {
+    "dataset" - {
+      "データセットの一覧が取得できるか" in {
+        session {
+          signIn()
+          val params = Map("limit" -> "10", "offset" -> "5")
+          get("/api/datasets", params) {
+            status should be(200)
+            val result = parse(body).extract[AjaxResponse[RangeSlice[DatasetsSummary]]]
+            println(result.data.summary)
+            result.data.summary.count should be(10)
+            result.data.summary.offset should be(5)
+          }
+        }
+      }
+
+      // TODO 複数ファイルを指定してデータセットを作成できるかどうか
+      "データセットが作成できるか" in {
+        session {
+          signIn()
+          val datasetId = createDataset()
+          get("/api/datasets/" + datasetId) {
+            checkStatus()
+            val result = parse(body).extract[AjaxResponse[Dataset]]
+            result.data.id should be (datasetId)
+          }
+        }
+      }
+
+      "作成したデータセットが削除できるか" in {
+        session {
+          signIn()
+          val datasetId = createDataset()
+          delete("/api/datasets/" + datasetId) { checkStatus() }
+          get("/api/datasets/" + datasetId) {
+            status should be(200)
+            val result = parse(body).extract[AjaxResponse[Dataset]]
+            result.status should be("NotFound")
+          }
+        }
+      }
+
+      // TODO attributesつきでの検証
+      "データセットの情報が編集できるか" in {
+        session {
+          signIn()
+          val datasetId = createDataset()
+          val params = Map(
+            "name" -> "変更後データセット",
+            "description" -> "change description",
+            "license" -> AppConf.defaultLicenseId
+          )
+          put("/api/datasets/" + datasetId + "/metadata", params) { checkStatus() }
+          get("/api/datasets/" + datasetId) {
+            checkStatus()
+            val result = parse(body).extract[AjaxResponse[Dataset]]
+            result.data.meta.name should be ("変更後データセット")
+          }
+        }
+      }
+
+      "データセットにファイルが追加できるか" in {
+        session {
+          signIn()
+          val datasetId = createDataset()
+          val files = Map("files[]" -> dummyFile)
+          val fileId = post("/api/datasets/" + datasetId + "/files", Map.empty, files) {
+            checkStatus()
+            val result = parse(body).extract[AjaxResponse[DatasetAddFiles]]
+            result.data.files(0).id
+          }
+          get("/api/datasets/" + datasetId) {
+            checkStatus()
+            val result = parse(body).extract[AjaxResponse[Dataset]]
+            result.data.filesCount should be(2)
+            assert(result.data.files.map(_.id).contains(fileId))
+          }
+        }
+      }
+
+      "データセットに追加したファイルが変更できるか" in {
+        session {
+          signIn()
+          val datasetId = createDataset()
+          val files = Map("files[]" -> dummyFile)
+          val fileId = post("/api/datasets/" + datasetId + "/files", Map.empty, files) {
+            checkStatus()
+            val result = parse(body).extract[AjaxResponse[DatasetAddFiles]]
+            result.data.files(0).id
+          }
+
+          val anotherFile = new File("build.sbt")
+          val anotherFileParam = Map("file" -> anotherFile)
+          post("/api/datasets/" + datasetId + "/files/" + fileId, Map.empty, anotherFileParam) {
+            println(body)
+            checkStatus()
+            val result = parse(body).extract[AjaxResponse[DatasetAddFiles]]
+            result.data.files(0).id should be(fileId)
+          }
+
+          get("/api/datasets/" + datasetId) {
+            checkStatus()
+            val result = parse(body).extract[AjaxResponse[Dataset]]
+            result.data.filesCount should be(2)
+            assert(result.data.files.map(_.id).contains(fileId))
+            result.data.files.map {x =>
+              if (x.id == fileId) {
+                x.size should be (anotherFile.length)
+              }
+            }
+          }
+        }
+      }
+
+      "データセットのファイル名が変更できるか" in {
+        session {
+          signIn()
+          val datasetId = createDataset()
+
+          // add files
+          val files = Map("files[]" -> dummyFile)
+          post("/api/datasets/" + datasetId + "/files", Map.empty, files) { checkStatus() }
+          val fileId = post("/api/datasets/" + datasetId + "/files", Map.empty, files) {
+            checkStatus()
+            val result = parse(body).extract[AjaxResponse[DatasetAddFiles]]
+            result.data.files(0).id
+          }
+          post("/api/datasets/" + datasetId + "/files", Map.empty, files) { checkStatus() }
+
+          val params = Map("name" -> "testtest.txt")
+          put("/api/datasets/" + datasetId + "/files/" + fileId + "/name", params) { checkStatus() }
+
+          get("/api/datasets/" + datasetId) {
+            checkStatus()
+            val result = parse(body).extract[AjaxResponse[Dataset]]
+            assert(result.data.files.map(_.id).contains(fileId))
+            result.data.files.map {x =>
+              if (x.id == fileId) {
+                x.name should be ("testtest.txt")
+              }
+            }
+          }
+        }
+      }
+
+      "データセットに追加したファイルが削除できるか" in {
+        session {
+          signIn()
+          val datasetId = createDataset()
+
+          // add files
+          val files = Map("files[]" -> dummyFile)
+          post("/api/datasets/" + datasetId + "/files", Map.empty, files) { checkStatus() }
+          val fileId = post("/api/datasets/" + datasetId + "/files", Map.empty, files) {
+            checkStatus()
+            val result = parse(body).extract[AjaxResponse[DatasetAddFiles]]
+            result.data.files(0).id
+          }
+          post("/api/datasets/" + datasetId + "/files", Map.empty, files) { checkStatus() }
+
+          delete("/api/datasets/" + datasetId + "/files/" + fileId) { checkStatus() }
+          get("/api/datasets/" + datasetId) {
+            checkStatus()
+            val result = parse(body).extract[AjaxResponse[Dataset]]
+            result.data.filesCount should be(3)
+            assert(!result.data.files.map(_.id).contains(fileId))
+          }
+        }
+      }
+
+      "データセットに画像が追加できるか" in {
+        session {
+          signIn()
+          val datasetId = createDataset()
+          val images = Map("images" -> dummyImage)
+          val imageId = post("/api/datasets/" + datasetId + "/images", Map.empty, images) {
+            checkStatus()
+            val result = parse(body).extract[AjaxResponse[DatasetAddImages]]
+            result.data.images(0).id
+          }
+          get("/api/datasets/" + datasetId) {
+            checkStatus()
+            val result = parse(body).extract[AjaxResponse[Dataset]]
+            result.data.images.size should be(2)
+            assert(result.data.images.map(_.id).contains(imageId))
+          }
+        }
+      }
+
+      "データセットに追加した画像が削除できるか" in {
+        session {
+          signIn()
+          val datasetId = createDataset()
+
+          // add images
+          val images = Map("images" -> dummyImage)
+          post("/api/datasets/" + datasetId + "/images", Map.empty, images) { checkStatus() }
+          val imageId = post("/api/datasets/" + datasetId + "/images", Map.empty, images) {
+            checkStatus()
+            val result = parse(body).extract[AjaxResponse[DatasetAddImages]]
+            result.data.images(0).id
+          }
+          post("/api/datasets/" + datasetId + "/images", Map.empty, images) { checkStatus() }
+
+          delete("/api/datasets/" + datasetId + "/images/" + imageId) {
+            checkStatus()
+            val result = parse(body).extract[AjaxResponse[DatasetDeleteImage]]
+            result.data.primaryImage should not be(imageId)
+          }
+          get("/api/datasets/" + datasetId) {
+            checkStatus()
+            val result = parse(body).extract[AjaxResponse[Dataset]]
+            result.data.images.size should be(3)
+            assert(!result.data.images.map(_.id).contains(imageId))
+          }
+        }
+      }
+
+      "データセットのメイン画像を変更できるか" in {
+        session {
+          signIn()
+          val datasetId = createDataset()
+
+          // add images
+          val images = Map("images" -> dummyImage)
+          post("/api/datasets/" + datasetId + "/images", Map.empty, images) { checkStatus() }
+          val imageId = post("/api/datasets/" + datasetId + "/images", Map.empty, images) {
+            checkStatus()
+            val result = parse(body).extract[AjaxResponse[DatasetAddImages]]
+            result.data.images(0).id
+          }
+          post("/api/datasets/" + datasetId + "/images", Map.empty, images) { checkStatus() }
+
+          // change primary image
+          val params = Map("id" -> imageId)
+          put("/api/datasets/" + datasetId + "/images/primary", params) { checkStatus() }
+
+          get("/api/datasets/" + datasetId) {
+            checkStatus()
+            val result = parse(body).extract[AjaxResponse[Dataset]]
+            result.data.primaryImage should be(imageId)
+          }
+        }
+      }
+
+      "データセットのファイルがダウンロードできるか" in {
+        session {
+          signIn()
+          val files = Map("file[]" -> dummyFile)
+          val url = post("/api/datasets", Map.empty, files) {
+            checkStatus()
+            parse(body).extract[AjaxResponse[Dataset]].data.files(0).url
+          }
+
+          println(url)
+          // FIXME 別途clientを使用してファイルDL検証
+          fail()
+        }
+      }
+
+      "データセットの画像がダウンロードできるか" in {
+        session {
+          signIn()
+          val datasetId = createDataset()
+          val images = Map("images" -> dummyImage)
+          val url = post("/api/datasets/" + datasetId + "/images", Map.empty, images) {
+            checkStatus()
+            val result = parse(body).extract[AjaxResponse[DatasetAddImages]]
+            result.data.images(0).url
+          }
+
+          println(url)
+          // FIXME 別途clientを使用して画像DL検証
+          fail()
+        }
+      }
+    }
+  }
+
+  private def signIn() {
+    val params = Map("id" -> "t_okada", "password" -> "password")
+    post("/api/signin", params) {
+      checkStatus()
+    }
+  }
+
+  private def checkStatus() {
+    status should be(200)
+    val result = parse(body).extract[AjaxResponse[Any]]
+    result.status should be("OK")
+  }
+
+  private def createDataset(): String = {
+    val files = Map("file[]" -> dummyFile)
+    post("/api/datasets", Map.empty, files) {
+      checkStatus()
+      parse(body).extract[AjaxResponse[Dataset]].data.id
+    }
+  }
+}
