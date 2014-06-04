b73aff99f07bfc36607c474dd981b15e7041a6fc
Takuya OKADA <t_okada@denkiyagi.jp>
2014-06-04 18:49:45 +0900

    [Server]グループメンバー一覧取得APIのメンバーのソート順を変更
    メンバーのrole順(降順)にソートするようにした

--- server/src/main/scala/dsmoq/services/GroupService.scala
+++ server/src/main/scala/dsmoq/services/GroupService.scala
@@ -761,7 +761,7 @@ val g = persistence.Group.syntax("g")
         .not.eq(m.role, GroupMemberRole.Deny)
         .and
         .isNull(m.deletedAt)
-        .orderBy(m.updatedAt).desc
+        .orderBy(m.role).desc
         .offset(offset)
         .limit(limit)
     }.map(rs => (persistence.User(u.resultName)(rs), rs.int(persistence.Member.column.role))).list().apply()
