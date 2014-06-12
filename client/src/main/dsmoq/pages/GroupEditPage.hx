dd7992ee24840cd8484c2b341506fc30bf1e4cbf
terurou <terurou@denkiyagi.jp>
2014-06-12 19:41:18 +0900

    [Client]グループ編集画面のブラッシュアップ

--- client/src/main/dsmoq/pages/GroupEditPage.hx
+++ client/src/main/dsmoq/pages/GroupEditPage.hx
@@ -1,5 +1,7 @@
 package dsmoq.pages;
 
+import js.Boot;
+import js.bootstrap.BootstrapButton;
 import js.support.ControllableStream;
 import js.html.Element;
 import js.jqhx.JqHtml;
@@ -10,19 +12,26 @@ import js.typeahead.Typeahead;
 import js.typeahead.Bloodhound;
 import dsmoq.models.Profile;
 import dsmoq.models.GroupMember;
+import dsmoq.framework.types.PageNavigation;
 import js.jqhx.JQuery;
 
 class GroupEditPage {
     public static function create(id: String) {
+        var engine = createAccountEngine();
+        var navigation = new ControllableStream();
+
         return {
-            navigation: new ControllableStream(),
+            navigation: navigation,
             invalidate: function (container: Element) {
                 var root = new JqHtml(container);
+                var rootBinding = JsViews.objectObservable({data: Async.Pending});
+                View.getTemplate("group/edit").link(container, rootBinding.data());
 
                 Service.instance.getGroup(id).then(function (res) {
                     var data = {
                         myself: Service.instance.profile,
                         group: res,
+                        groupErrors: {description: ""},
                         isMembersLoading: true,
                         members: {
                             summary: { count: 0, offset: 0, total: 0 },
@@ -30,20 +39,7 @@ class GroupEditPage {
                         },
                     };
                     var binding = JsViews.objectObservable(data);
-
-                    View.getTemplate("group/edit").link(container, binding.data());
-
-                    var engine = new Bloodhound<Profile>({
-                        datumTokenizer: Bloodhound.tokenizers.obj.whitespace("name"),
-                        queryTokenizer: Bloodhound.tokenizers.whitespace,
-                        prefetch: {
-                            url: "/api/accounts",
-                            filter: function (x) {
-                                return x.data;
-                            }
-                        }
-                    });
-                    engine.initialize();
+                    rootBinding.setProperty("data", Async.Completed(data));
 
                     Typeahead.initialize(root.find("#group-user-typeahead"), {}, {
                         source: engine.ttAdapter(),
@@ -67,7 +63,22 @@ class GroupEditPage {
                     });
 
                     root.find("#group-basics-submit").on("click", function (_) {
-                        Service.instance.updateGroupBasics(id, data.group.name, data.group.description);
+                        BootstrapButton.setLoading(root.find("#group-basics-submit"));
+                        root.find("#group-basics").find("input,textarea").attr("disabled", true);
+                        Service.instance.updateGroupBasics(id, data.group.name, data.group.description).then(function (_) {
+                            Notification.show("success", "save successful");
+                        }, function (err) {
+                            switch (err.name) {
+                                case ServiceErrorType.BadRequest:
+                                    for (x in cast(err, ServiceError).detail) {
+                                        binding.setProperty('groupErrors.${x.name}', x.message);
+                                    }
+                            }
+                            Notification.show("error", "error happened");
+                        }, function () {
+                            BootstrapButton.reset(root.find("#group-basics-submit"));
+                            root.find("#group-basics").find("input,textarea").removeAttr("disabled");
+                        });
                     });
 
                     root.find("#group-icon-form").on("change", "input[type=file]", function (e) {
@@ -78,13 +89,21 @@ class GroupEditPage {
                         }
                     });
                     root.find("#group-icon-submit").on("click", function (_) {
+                        BootstrapButton.setLoading(root.find("#group-icon-submit"));
                         Service.instance.changeGroupImage(id, JQuery.find("#group-icon-form")).then(function (res) {
                             var img = res.images.filter(function (x) return x.id == res.primaryImage)[0];
                             binding.setProperty("group.primaryImage.id", img.id);
                             binding.setProperty("group.primaryImage.url", img.url);
                             root.find("#group-icon-form input[type=file]").val("");
                             root.find("#group-icon-submit").hide();
+                            Notification.show("success", "save successful");
+                        }, function (err) {
+                            Notification.show("error", "error happened");
+                        }, function () {
+                            BootstrapButton.reset(root.find("#group-icon-submit"));
+                            root.find("#group-icon input").removeAttr("disabled");
                         });
+                        root.find("#group-icon input").attr("disabled", true);
                     });
 
                     root.find("#group-user-add").on("click", function (_) {
@@ -108,10 +127,29 @@ class GroupEditPage {
                     root.find("#group-member-submit").on("click", function (_) {
                         Service.instance.updateGroupMemberRoles(id, data.members.results);
                     });
+
+                    root.find("#group-finish-editing").on("click", function (_) {
+                        navigation.update(PageNavigation.Navigate(Page.GroupShow(id)));
+                    });
                 });
             },
             dispose: function () {
             }
         }
+    }
+
+    static function createAccountEngine() {
+        var engine = new Bloodhound<Profile>({
+            datumTokenizer: Bloodhound.tokenizers.obj.whitespace("name"),
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            prefetch: {
+                url: "/api/accounts",
+                filter: function (x) {
+                    return x.data;
+                }
+            }
+        });
+        engine.initialize();
+        return engine;
     }
 }
