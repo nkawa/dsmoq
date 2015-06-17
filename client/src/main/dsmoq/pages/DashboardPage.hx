e0d5ccb30b6fc64305ea2753c9b7b29c91b5544d
S.Soyama <s_soyama@denkiyagi.jp>
2015-06-17 19:34:33 +0900

    Welcomeメッセージをguestユーザーには表示しないよう修正

--- client/src/main/dsmoq/pages/DashboardPage.hx
+++ client/src/main/dsmoq/pages/DashboardPage.hx
@@ -16,26 +16,27 @@ import js.html.Location;
 
 class DashboardPage {
 
-    public static function render(html: Html, onClose: Promise<Unit>): Promise<Navigation<Page>> {
-        var profile = Service.instance.profile;
-
+	public static function render(html: Html, onClose: Promise<Unit>): Promise<Navigation<Page>> {
+		var profile = Service.instance.profile;
+		if (profile.isGuest) {
+			js.Browser.location.href = "/";
+			return new Promise(function (_) { });
+		}
 		var rootBinding = JsViews.observable({ data: dsmoq.Async.Pending });
 		View.getTemplate("dashboard/show").link(html, rootBinding.data());
 
-		Service.instance.getTags().then(function(x) {
-			var data = {
-				isGuest: profile.isGuest,
-				myDatasets: Async.Pending,
-				message: Async.Pending,
-				tag: x
-			};
-
-            rootBinding.setProperty("data", data);
-            var binding = JsViews.observable(rootBinding.data().data);
-
-			if (!profile.isGuest) {
-				Service.instance.findDatasets({owners: [profile.name], limit: 12}).then(function (x) {
-					binding.setProperty("myDatasets", Async.Completed(x.results));
+		Service.instance.getTags().then(function(tags) {
+			Service.instance.getMessage().then(function(message) {
+				var data = {
+					isGuest: profile.isGuest,
+					myDatasets: Async.Pending,
+					message: message,
+					tag: tags
+				};
+				rootBinding.setProperty("data", data);
+				var binding = JsViews.observable(rootBinding.data().data);
+				Service.instance.findDatasets({owners: [profile.name], limit: 12}).then(function (dataset) {
+					binding.setProperty("myDatasets", Async.Completed(dataset.results));
 					JQuery._(".dataset-title").each(function(i, dom) {
 						var el = JQuery._(dom);
 						var s = el.text();
@@ -44,14 +45,8 @@ class DashboardPage {
 						}
 					});
 				});
-				
-				Service.instance.getMessage().then(function(x) {
-					binding.setProperty("message", Async.Completed(x));
-				});
-			}
+			});
 		});
-		
-        return new Promise(function (_) { });
-    }
-
+		return new Promise(function (_) { });
+	}
 }
