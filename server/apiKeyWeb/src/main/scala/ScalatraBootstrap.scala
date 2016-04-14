53e741460889020088388ef62f0cd38128754048
Kenji Nagase <androxion@gmail.com>
2016-04-14 16:42:08 +0900

    [No.06] APIキー発行ツール DBライブラリ初期化
    - DBライブラリの初期化・終了処理の実装

--- server/apiKeyWeb/src/main/scala/ScalatraBootstrap.scala
+++ server/apiKeyWeb/src/main/scala/ScalatraBootstrap.scala
@@ -1,9 +1,17 @@
+import javax.servlet.ServletContext
+
 import dsmoq.apikeyweb._
 import org.scalatra._
-import javax.servlet.ServletContext
+import scalikejdbc.config.DBs
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
     context.mount(new MainServlet, "/*")
+
+    DBs.setup()
+  }
+
+  override def destroy(context: ServletContext) {
+    DBs.close()
   }
 }
