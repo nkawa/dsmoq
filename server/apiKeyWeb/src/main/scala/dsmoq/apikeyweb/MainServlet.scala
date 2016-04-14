5d0d1a0bde29bea39f5b16289c856a79f84555bc
Kenji Nagase <androxion@gmail.com>
2016-04-14 18:47:59 +0900

    [No.06] APIキー発行ツール 画面の実装
    - テンプレートによるユーザーID入力、結果出力画面の実装

--- server/apiKeyWeb/src/main/scala/dsmoq/apikeyweb/MainServlet.scala
+++ server/apiKeyWeb/src/main/scala/dsmoq/apikeyweb/MainServlet.scala
@@ -1,27 +1,60 @@
 package dsmoq.apikeyweb
 
+import java.util.UUID
+
+import dsmoq.persistence.{ApiKey, User}
+import org.apache.commons.codec.digest.DigestUtils
+import org.joda.time.DateTime
+import scalikejdbc._
+
 class MainServlet extends ApiKeyWebToolStack {
+  val systemUserId = "dccc110c-c34f-40ed-be2c-7e34a9f1b8f0"
 
   get("/") {
     contentType = "text/html"
-
-    <html>
-      <body>
-        <h1>API発行ツール</h1>
-        <form action="/publicsh" method="post">
-          <p>
-            <label>ユーザー名</label>
-            <input name="user_name" type="text"/>
-          </p>
-          <p>
-            <input type="submit" value="発行"/>
-          </p>
-        </form>
-      </body>
-    </html>
+    layoutTemplate("/WEB-INF/templates/views/index.ssp", "userName" -> "", "errorMessage" -> "")
   }
 
-  post("/publicsh") {
+  get("/error/:username") {
+    val userName = params("username")
+    val msg = "ユーザ \"%s\" は存在しません。".format(userName)
+
+    contentType = "text/html"
+    layoutTemplate("/WEB-INF/templates/views/index.ssp", "userName" -> userName, "errorMessage" -> msg)
+  }
+
+  post("/publish") {
+    val userName = params("user_name")
+
+    DB localTx { implicit s =>
+      val u = User.u
+      val userId = withSQL {
+        select(u.result.id).from(User as u).where.eq(u.name, userName)
+      }.map(rs => rs.string(u.resultName.id)).single.apply
+
+      val timestamp = DateTime.now
+      userId match {
+        case Some(uId) =>
+          val apiKey = DigestUtils.sha256Hex(UUID.randomUUID().toString)
+          val secretKey = DigestUtils.sha256Hex(UUID.randomUUID().toString + apiKey)
+          ApiKey.create(
+            id = UUID.randomUUID().toString,
+            userId = uId,
+            apiKey = apiKey,
+            secretKey = secretKey,
+            permission = 3,
+            createdBy = systemUserId,
+            createdAt = timestamp,
+            updatedBy = systemUserId,
+            updatedAt = timestamp
+          )
+          contentType = "text/html"
+          layoutTemplate("/WEB-INF/templates/views/result.ssp",
+            "userName" -> userName, "consumerKey" -> apiKey, "secretKey" -> secretKey)
+        case _ =>
+          redirect("/error/" + userName)
+      }
+    }
   }
 
 }
--- server/project/build.scala
+++ server/project/build.scala
@@ -160,6 +160,7 @@ object DsmoqBuild extends Build {
         "ch.qos.logback" % "logback-classic" % "1.1.5" % "runtime",
         "org.eclipse.jetty" % "jetty-webapp" % "9.2.15.v20160210" % "container;compile",
         "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
+        "commons-codec" % "commons-codec" % "1.10",
         "com.typesafe" % "config" % "1.3.0"
       ),
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile) { base =>
