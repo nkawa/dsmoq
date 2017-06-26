01d59ab70ba8b153e0d33fba0ab502651b1d00a8
Kenji Nagase <knagase@adotplus.com>
2017-06-26 14:00:09 +0900

    ローカルユーザー作成機能
    - 不正な値を入力したときメッセージを表示するよう修正

--- server/maintenance/src/main/webapp/js/localuser/index.js
+++ server/maintenance/src/main/webapp/js/localuser/index.js
@@ -1,5 +1,23 @@
 $(function() {
-    $("button.create_user").click(function() {
+    $("#localUserForm").submit(function(event) {
+        var userId = $("input#userName");
+        if (userId.validity.valueMissing) {
+            userId.setCustomValidity("必須入力項目です");
+            return false;
+        } else if (userId.validity.patternMismatch) {
+            userId.setCustomValidity("半角英数字で入力してください");
+            return false;
+        } else {
+           userId.setCustomValidity("");
+        }
+        var password = $("input#password");
+        if (password.validity.valueMissing) {
+            password.setCustomValidity("必須入力項目です");
+            return false;
+        } else {
+           password.setCustomValidity("");
+        }
+
         if (!confirm("ユーザーを作成します。よろしいでしょうか？")) {
             return false;
         }
